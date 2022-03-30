af50e85edf2d0878a54543215b4511f8b8288bc9
Daniel Göthe <daniel@goethe.se>
2022-03-30 11:23:55 +0200

    added Activitys and fragments
    added Activitys and fragments

---
+++ mower-app/app/src/main/java/com/example/myapplication/ManualDriveFragment.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+
+class ManualDriveFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstantState: Bundle?
+    ): View? {
+        return null
+    }
+}
