b1d35c6ba8991f328ee1aac6a0789ee7780fc2f4
manzuki <kasi18jk@student.ju.se>
2022-03-31 16:18:20 +0200

    Fragments - not tested

--- mower-app/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,60 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,
+    BottomNavigationView.OnNavigationItemSelectedListener
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bundle = Bundle()
+        val navController = findNavController(findViewById(R.id.fragment))
+        navController.navigate(R.id.MapFragment, bundle)
+        val navView: BottomNavigationView = findViewById(R.id.MapFragment)
+        navView.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.ManualDriveFragment -> {
+                val bundle = Bundle()
+                //bundle.putString(GROUPID, groupId)
+                val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+                val navController = findNavController(findViewById(R.id.fragment))
+                navView.setupWithNavController(navController)
+                navController.navigate(R.id.ManualDriveFragment, bundle)
+            }
+
+            R.id.MapFragment -> {
+                val bundle = Bundle()
+                //bundle.putString(GROUPID, groupId)
+                val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+                val navController = Navigation.findNavController(findViewById(R.id.fragment))
+                navController.saveState()
+                navView.setupWithNavController(navController)
+                navController.navigate(R.id.MapFragment, bundle)
+            }
+
+            R.id.SettingsFragment -> {
+                val bundle = Bundle()
+                //bundle.putString(GROUPID, groupId)
+                val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+                val navController = Navigation.findNavController(findViewById(R.id.fragment))
+                navController.saveState()
+                navView.setupWithNavController(navController)
+                navController.navigate(R.id.SettingsFragment, bundle)
+            }
+        }
+        return true
     }
 }
--- mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class SelectionActivity: AppCompatActivity() {
+class SelectionActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selection)
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
     }
 }
