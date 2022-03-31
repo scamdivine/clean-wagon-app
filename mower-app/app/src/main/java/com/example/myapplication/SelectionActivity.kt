43d3e2958dda48bd1012a2c5182d6aa8d9174178
Daniel Göthe <daniel@goethe.se>
2022-03-31 17:35:11 +0200

    merged branches and resolved conflicts

--- mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SelectionActivity : AppCompatActivity() {
