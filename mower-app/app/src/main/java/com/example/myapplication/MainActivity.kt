b669c4b024df9c4e6913cd1ae797eb8039e16da0
Daniel Göthe <daniel@goethe.se>
2022-03-31 15:03:35 +0200

    navigation between activitys
    navigation between activitys(might change to work with fragments)

--- mower-app/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var testButton = findViewById<Button>(R.id.testButton)
+
+        testButton.setOnClickListener(){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
--- mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SelectionActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection)
+
+        var selectMowerButton = findViewById<Button>(R.id.selectMowerButton)
+
+        selectMowerButton.setOnClickListener(){
+            val intent =  Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
