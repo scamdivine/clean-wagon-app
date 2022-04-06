428e5e1d5dedf308ec2de67728266a4506f9be82
scamdivine <72702048+scamdivine@users.noreply.github.com>
2022-04-06 10:00:21 +0200

    started with design on selection screen
    started with design on selection screen

--- mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
@@ -1,11 +1,12 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
-import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class SelectionActivity : AppCompatActivity() {
 
@@ -14,7 +15,22 @@ class SelectionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_selection)
 
         var selectMowerButton = findViewById<Button>(R.id.selectMowerButton)
+        var mowerTextView = findViewById<TextView>(R.id.mowerTextView)
+        var statusTextView = findViewById<TextView>(R.id.statusTextView)
+        var currentMower = "Husqvarna Automower 105"
+        var isActive = false
+        statusTextView.setBackgroundResource(R.drawable.active_background);
 
+
+        val drawable = statusTextView.getBackground()
+        if (isActive) {
+            drawable.colorFilter(Color.RED)
+        } else {
+            drawable.setColor(Color.BLUE)
+        }
+
+
+        mowerTextView.text = currentMower
         selectMowerButton.setOnClickListener(){
             val intent =  Intent(this, MainActivity::class.java)
             startActivity(intent)
