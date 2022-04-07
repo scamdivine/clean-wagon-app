e156f6df88715574dcd80d4bb2173e0f49d4b69a
Daniel Göthe <daniel@goethe.se>
2022-04-07 15:56:33 +0200

    added design to select mower screen
    added design to select mower activity
    - changeing color of mower activity
    - color and image on battery text
    - placeholder image for mower

--- mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
@@ -1,36 +1,48 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class SelectionActivity : AppCompatActivity() {
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection)
 
         var selectMowerButton = findViewById<Button>(R.id.selectMowerButton)
         var mowerTextView = findViewById<TextView>(R.id.mowerTextView)
-        var statusTextView = findViewById<TextView>(R.id.statusTextView)
+        var mowerStatus = findViewById<ImageView>(R.id.MowerStatus)
+        var batteryTextView = findViewById<TextView>(R.id.batteryTextView)
         var currentMower = "Husqvarna Automower 105"
-        var isActive = false
-        statusTextView.setBackgroundResource(R.drawable.active_background);
-
-
-        val drawable = statusTextView.getBackground()
-        if (isActive) {
-            drawable.colorFilter(Color.RED)
+        var batteryLvl = 75
+        var batteryPresent = batteryLvl.toString() + "% "
+        var isActive = true
+        if(isActive){
+            mowerStatus.setImageResource(R.drawable.active_status)
         } else {
-            drawable.setColor(Color.BLUE)
+            mowerStatus.setImageResource(R.drawable.inactive_status)
         }
 
-
+        batteryTextView.text = batteryPresent
         mowerTextView.text = currentMower
+        if (batteryLvl>50){
+            batteryTextView.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.battery_full,0)
+            batteryTextView.setTextColor(Color.parseColor("#95E05B"))
+        } else if (batteryLvl>20){
+            batteryTextView.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.battery_half,0)
+            batteryTextView.setTextColor(Color.parseColor("#F2C759"))
+        } else {
+            batteryTextView.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.battery_low,0)
+            batteryTextView.setTextColor(Color.parseColor("#F24E1E"))
+        }
         selectMowerButton.setOnClickListener(){
             val intent =  Intent(this, MainActivity::class.java)
             startActivity(intent)
