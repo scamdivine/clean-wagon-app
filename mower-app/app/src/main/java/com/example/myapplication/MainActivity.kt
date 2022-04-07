1e4815768eb7a8d0044f26d6f94f2112fc0e2b9a
Alexis Leloup <alexis.leloup@epitech.eu>
2022-04-07 15:48:48 +0200

    Adding images

--- mower-app/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,14 @@
 package com.example.myapplication
 
 import android.content.Intent
-import android.widget.Button
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication.R.id.fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() ,
@@ -19,46 +20,48 @@ class MainActivity : AppCompatActivity() ,
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var testButton = findViewById<Button>(R.id.testButton)
 
-        testButton.setOnClickListener(){
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
         val bundle = Bundle()
-        val navController = findNavController(findViewById(R.id.fragment))
-        navController.navigate(R.id.MapFragment, bundle)
+        val navController = findNavController(fragment)
         val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navController.navigate(R.id.offlineFragment, bundle)
         navView.setOnNavigationItemSelectedListener(this)
+
+        val returnButton = findViewById<ImageView>(R.id.returnArrow)
+        returnButton.setOnClickListener{
+            startActivity(Intent(this, SelectionActivity::class.java))
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
         when (item.itemId) {
+
+            R.id.MapFragment -> {
+                //val bundle = Bundle()
+                //bundle.putString(GROUPID, groupId)
+                val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+                val navController = findNavController(findViewById(fragment))
+                navController.saveState()
+                navView.setupWithNavController(navController)
+                navController.navigate(R.id.MapFragment, Bundle())
+            }
+
             R.id.ManualDriveFragment -> {
                 val bundle = Bundle()
                 //bundle.putString(GROUPID, groupId)
                 val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-                val navController = findNavController(findViewById(R.id.fragment))
-                navView.setupWithNavController(navController)
-                navController.navigate(R.id.ManualDriveFragment, bundle)
-            }
-
-            R.id.MapFragment -> {
-                val bundle = Bundle()
-                //bundle.putString(GROUPID, groupId)
-                val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-                val navController = Navigation.findNavController(findViewById(R.id.fragment))
+                val navController = findNavController(findViewById(fragment))
                 navController.saveState()
                 navView.setupWithNavController(navController)
-                navController.navigate(R.id.MapFragment, bundle)
+                navController.navigate(R.id.ManualDriveFragment, bundle)
             }
 
             R.id.SettingsFragment -> {
                 val bundle = Bundle()
                 //bundle.putString(GROUPID, groupId)
                 val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-                val navController = Navigation.findNavController(findViewById(R.id.fragment))
+                val navController = findNavController(findViewById(fragment))
                 navController.saveState()
                 navView.setupWithNavController(navController)
                 navController.navigate(R.id.SettingsFragment, bundle)
--- mower-app/app/src/main/java/com/example/myapplication/SettingsFragment.kt
+++ mower-app/app/src/main/java/com/example/myapplication/SettingsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class SettingsFragment: Fragment() {
@@ -12,6 +14,9 @@ class SettingsFragment: Fragment() {
         container: ViewGroup?,
         savedInstantState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        view.findViewById<TextView>(R.id.mowerName).text = "HUSQVARNA AUTOMOWER® 450X";
+        view.findViewById<ImageView>(R.id.mowerImage).setImageResource(R.drawable.facebook_logo)
         return null
     }
 }
