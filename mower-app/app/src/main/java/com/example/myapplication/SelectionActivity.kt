ee2c12108e4a56e2c7cfcfdc63bb498cf2978cbe
scamdivine <72702048+scamdivine@users.noreply.github.com>
2022-04-12 19:45:49 +0200

    buttons to swap mower
    Created buttons to swap mower left and right with logic for when to display

--- mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
+++ mower-app/app/src/main/java/com/example/myapplication/SelectionActivity.kt
@@ -17,20 +17,30 @@ class SelectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection)
 
-        var selectMowerButton = findViewById<Button>(R.id.selectMowerButton)
-        var mowerTextView = findViewById<TextView>(R.id.mowerTextView)
-        var mowerStatus = findViewById<ImageView>(R.id.MowerStatus)
-        var batteryTextView = findViewById<TextView>(R.id.batteryTextView)
+        val allMowers = arrayOf(1, 2, 3, 4)
+        val selectMowerButton = findViewById<Button>(R.id.selectMowerButton)
+        val mowerTextView = findViewById<TextView>(R.id.mowerTextView)
+        val mowerStatus = findViewById<ImageView>(R.id.MowerStatus)
+        val batteryTextView = findViewById<TextView>(R.id.batteryTextView)
+        val leftArrow = findViewById<ImageView>(R.id.arrowLeft)
+        val rightArrow = findViewById<ImageView>(R.id.arrowRight)
         var currentMower = "Husqvarna Automower 105"
         var batteryLvl = 75
         var batteryPresent = batteryLvl.toString() + "% "
         var isActive = true
+        var currentMowerCount = 1
+        if (currentMowerCount == 1){
+            leftArrow.alpha = 0.0f
+        }
+        if (allMowers.size<2){
+            leftArrow.alpha = 0.0f
+            rightArrow.alpha = 0.0f
+        }
         if(isActive){
             mowerStatus.setImageResource(R.drawable.active_status)
         } else {
             mowerStatus.setImageResource(R.drawable.inactive_status)
         }
-
         batteryTextView.text = batteryPresent
         mowerTextView.text = currentMower
         if (batteryLvl>50){
@@ -42,6 +52,27 @@ class SelectionActivity : AppCompatActivity() {
         } else {
             batteryTextView.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.battery_low,0)
             batteryTextView.setTextColor(Color.parseColor("#F24E1E"))
+        }
+
+        leftArrow.setOnClickListener {
+            if (currentMowerCount > 1){
+                currentMowerCount -= 1
+                rightArrow.alpha = 1.0f
+            }
+            if (currentMowerCount == 1){
+                leftArrow.alpha = 0.0f
+            }
+            println(currentMowerCount)
+        }
+        rightArrow.setOnClickListener {
+            if (currentMowerCount < allMowers.size){
+                currentMowerCount += 1
+                leftArrow.alpha = 1.0f
+            }
+            if (currentMowerCount == allMowers.size) {
+                rightArrow.alpha = 0.0f
+            }
+            println(currentMowerCount)
         }
         selectMowerButton.setOnClickListener(){
             val intent =  Intent(this, MainActivity::class.java)
