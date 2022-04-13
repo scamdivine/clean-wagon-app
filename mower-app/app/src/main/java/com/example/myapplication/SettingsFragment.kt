5b6c5dd3b6d8ab9de27b4507365578a28cc8a927
Alexis Leloup <alexis.leloup@epitech.eu>
2022-04-13 11:43:24 +0200

    Adding setting fragment completed view

--- mower-app/app/src/main/java/com/example/myapplication/SettingsFragment.kt
+++ mower-app/app/src/main/java/com/example/myapplication/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,12 +15,35 @@ class SettingsFragment: Fragment() {
         container: ViewGroup?,
         savedInstantState: Bundle?
     ): View? {
+        val batteryPercentage = 24;
+        val mowerName = "HUSQVARNA AUTOMOWER® 450X"
+        val isActive = true
+        val mowerImage = R.drawable.mower_105;
+
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        val batteryImageView = view.findViewById<ImageView>(R.id.batteryImage)
+        val batteryTextView = view.findViewById<TextView>(R.id.batteryPercentage)
+        val statusImageView = view.findViewById<ImageView>(R.id.statusImage)
+
         view.findViewById<TextView>(R.id.mowerName).text = "HUSQVARNA AUTOMOWER® 450X";
-        view.findViewById<ImageView>(R.id.mowerImage).setImageResource(R.drawable.facebook_logo)
-        view.findViewById<ImageView>(R.id.batteryImage).setImageResource(R.drawable.facebook_logo)
-        view.findViewById<ImageView>(R.id.statusImage).setImageResource(R.drawable.facebook_logo)
-        view.findViewById<TextView>(R.id.batteryPercentage).text = "82%"
+        view.findViewById<ImageView>(R.id.mowerImage).setImageResource(mowerImage)
+        view.findViewById<TextView>(R.id.batteryPercentage).text = batteryPercentage.toString() + "%";
+
+        if (isActive)
+            statusImageView.setImageResource(R.drawable.active_status)
+        else
+            statusImageView.setImageResource(R.drawable.inactive_status)
+
+        if (batteryPercentage >= 50) {
+            batteryImageView.setImageResource(R.drawable.battery_full)
+            batteryTextView.setTextColor(Color.parseColor("#95E05B"))
+        } else if (batteryPercentage < 50 && batteryPercentage >= 20) {
+            batteryImageView.setImageResource(R.drawable.battery_half)
+            batteryTextView.setTextColor(Color.parseColor("#F2C759"))
+        } else {
+            batteryImageView.setImageResource(R.drawable.battery_low)
+            batteryTextView.setTextColor(Color.parseColor("#F24E1E"))
+        }
         return view
     }
 }
